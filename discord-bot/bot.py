1af49dcca37e1f08e0316563d7fe3749c54c9d41
Sebastien Lavoie <slavoie689@gmail.com>
2021-05-23 13:19:01 -0400

    bot: Basic connection to discord

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-from configparser import ConfigParser 
+import discord
+from configparser import ConfigParser
 from pathlib import Path
 
 config = ConfigParser()
-config.read()
+config.read(Path.home().joinpath(".discord-creds.conf"))
+
+token = config['default'].token
+
+client = discord.client()
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name is "MC Server":
+            break
+
+    print(f"{client.user} has connected to guild {guild.name} with id {guild.id}")
+
+client.run(token)
