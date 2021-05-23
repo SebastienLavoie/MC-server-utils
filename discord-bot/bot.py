55f3e96bb06de7ec0724b1cf903b6ab6f78e2067
Sebastien Lavoie <slavoie689@gmail.com>
2021-05-23 13:31:18 -0400

    bot: respond to hello

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -7,17 +7,24 @@ from pathlib import Path
 config = ConfigParser()
 config.read(Path.home().joinpath(".discord-creds.conf"))
 
-token = config['default'].token
+token = config['default']["token"]
 
-client = discord.client()
+client = discord.Client()
 
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name is "MC Server":
+        if guild.name == "MC Server":
             break
 
     print(f"{client.user} has connected to guild {guild.name} with id {guild.id}")
+
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == "hello":
+        message.channel.send("Hello World!")
+
 
 client.run(token)
