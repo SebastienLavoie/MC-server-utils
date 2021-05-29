8cc98db7b2b847d63b4dc756cb30ae275be916f9
Sebastien Lavoie <slavoie689@gmail.com>
2021-05-29 17:34:41 -0400

    bot: add intents and task for adding online role

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import discord
+from discord.ext import tasks, commands
 
 import logging
 import re
+from typing import Dict
 from sys import stdout
 from configparser import ConfigParser
 from pathlib import Path
@@ -12,6 +14,7 @@ from subprocess import run, PIPE
 num_players = 3
 response_channel = "server-evenements"
 
+# Logging setup
 file_name = Path(__file__).stem
 log = logging.getLogger(file_name)
 
@@ -23,27 +26,24 @@ handler.setFormatter(formatter)
 log.addHandler(handler)
 log.setLevel(logging.DEBUG)
 
+# Config reading
 config = ConfigParser()
-config.read(Path.home().joinpath(".discord-creds.conf"))
+config.read(Path.home().joinpath(".config", "discord", "mcserverbot.conf"))
 
 token = config['default']["token"]
+online_role_id = config["default"]["online_role_id"]
 server_log = Path.home().joinpath("minecraft", "create-mod", "server.log")
 
-client = discord.Client()
+# Intents
+intents = discord.Intents(messages=True, members=True)
 
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == "MC Server":
-            break
+class MCServerClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    log.info(f"{client.user} has connected to guild {guild.name} with id {guild.id}")
-
-
-@client.event
-async def on_message(message):
-    def online() -> dict:
+    @staticmethod
+    def online() -> Dict[str, bool]:
         players = dict()
         with open(server_log, "r") as fp:
             for line in reversed(fp.readlines()):
@@ -58,34 +58,84 @@ async def on_message(message):
                     break
         return players
 
-    if message.channel.name == response_channel:
-        log.debug(f"Received {message.content} from {message.author}")
-        if message.content.lower() == "hello":
-            msg = "Hello World!"
-            log.info(f"Sending: {msg}")
-            await message.channel.send(msg)
-        elif message.content.lower() == "!help":
-            msg = "Available commands: !ip, !online"
-            log.info(f"Sending: {msg}")
-            await message.channel.send(msg)
-        elif message.content.lower() == "!ip":
-            ip = run("dig +short myip.opendns.com @resolver1.opendns.com", shell=True, stdout=PIPE, universal_newlines=True)
-            msg = ip.stdout
-            log.info(f"Sending: {msg}")
-            await message.channel.send(msg)
-        elif message.content.lower() == "!online":
-            players = online()
-            if len(players) == 0:
-                msg = "No one online"
-                log.info(f"Sending: {msg}")
-                await message.channel.send(msg)
-            else:
-                msg = ""
-                for player, online in players.items():
+    @staticmethod
+    def get_ip() -> str:
+        return run("dig +short myip.opendns.com @resolver1.opendns.com", shell=True, stdout=PIPE,
+                   universal_newlines=True).stdout
+
+    def get_roles(self):
+        return self.guilds[0].roles
+
+    def get_online_role(self) -> discord.Role:
+        roles = self.get_roles()
+        for role in roles:
+            if role.id == online_role_id:
+                return role
+
+        raise discord.ClientException("Could not find online role in guild!")
+
+    def get_members_dict(self) -> Dict[str, discord.Member]:
+        members = self.guilds[0].members
+        return {m.nick.lower(): m for m in members}
+
+    @tasks.loop(minutes=5.0)
+    async def update_player_status(self):
+        players_online = self.online()
+        log.debug(players_online)
+        members_dict = self.get_members_dict()
+        log.debug(members_dict)
+        online_role = self.get_online_role()
+
+        if len(players_online) == 0:
+            return
+        else:
+            for player, online in players_online.items():
+                if player.lower() in members_dict.keys():
                     if online is True:
-                        msg += f"{player}\n"
-                msg = "No one online" if len(msg) == 0 else msg
+                        log.info(f"Adding role {online_role.name} to {player}")
+                        await members_dict[player.lower()].add_roles(online_role)
+                    elif online_role in members_dict[player.lower()].roles and online is False:
+                        log.info(f"Removing role {online_role.name} from {player}")
+                        await members_dict[player.lower()].remove_roles(online_role)
+
+    async def on_ready(self):
+        for guild in self.guilds:
+            if guild.name == "MC Server":
+                break
+
+        log.info(f"{self.user} has connected to guild {guild.name} with id {guild.id}")
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.channel.name == response_channel:
+            log.debug(f"Received {message.content} from {message.author}")
+            if message.content.lower() == "hello":
+                msg = "Hello World!"
                 log.info(f"Sending: {msg}")
                 await message.channel.send(msg)
+            elif message.content.lower() == "!help":
+                msg = "Available commands: !ip, !online"
+                log.info(f"Sending: {msg}")
+                await message.channel.send(msg)
+            elif message.content.lower() == "!ip":
+                msg = self.get_ip()
+                log.info(f"Sending: {msg}")
+                await message.channel.send(msg)
+            elif message.content.lower() == "!online":
+                players = self.online()
+                if len(players) == 0:
+                    msg = "No one online"
+                    log.info(f"Sending: {msg}")
+                    await message.channel.send(msg)
+                else:
+                    msg = ""
+                    for player, online in players.items():
+                        if online is True:
+                            msg += f"{player}\n"
+                    msg = "No one online" if len(msg) == 0 else msg
+                    log.info(f"Sending: {msg}")
+                    await message.channel.send(msg)
 
 client.run(token)
