a6760498710483d74d93718cfd27689502b5a992
slavoie <slavoie689@gmail.com>
2021-08-14 16:52:02 -0400

    backup_server: don't do deletions in dry run

--- backup_server.sh
+++ backup_server.sh
@@ -72,8 +72,10 @@ if [ -e /tmp/mc-backup ]; then
     rm -rf /tmp/mc-backup
 fi
 mkdir /tmp/mc-backup
+echo "Copying to /tmp ..."
 cp -a "$SERVER_DIR"/world /tmp/mc-backup/
 if [ ! "$dry_run" ]; then
+  echo "Compressing..."
   tar cf "$BACKUP_DIR"/"$SERVER_NAME"-"$DATE_STR".tar.gz /tmp/mc-backup/world
 fi
 rm -rf /tmp/mc-backup
@@ -91,6 +93,8 @@ exec_time=$(("$stop_time" - "$start_time"))
 file_size=$(du -h "$BACKUP_DIR"/"$SERVER_NAME"-"$DATE_STR".tar.gz | cut -f1)
 server_say "Upload Complete In ${exec_time}s. File size ${file_size}"
 echo "Upload Complete In ${exec_time}s. File size ${file_size}"
-delete_files_older_than "$LOCAL_EXPIRY_DAYS" "$BACKUP_DIR"
-# delete_files_older_than "$CLOUD_EXPIRY_DAYS" "$CLOUD_BACKUP_DIR"
+if [ ! "$dry_run" ]; then
+  delete_files_older_than "$LOCAL_EXPIRY_DAYS" "$BACKUP_DIR"
+  # delete_files_older_than "$CLOUD_EXPIRY_DAYS" "$CLOUD_BACKUP_DIR"
+fi
 echo "---------------------------------------------------------------------"
