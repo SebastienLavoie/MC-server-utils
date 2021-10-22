24c1f62d01bee006b97a41e3c8e9571f95d966d9
slavoie <slavoie689@gmail.com>
2021-10-21 23:06:10 -0400

    make valheim start server script actually work and remove password
    Cause yes I'm a fucking idiot who commited a password. I changed it
    since though :)

--- valheim/start_server.sh
+++ valheim/start_server.sh
@@ -1,4 +1,10 @@
-docker run -d --name valheim-server --cap-add=sys_nice --stop-timeout 120 \
+#!/bin/bash
+
+set -eEuo pipefail
+
+source /home/slavoie/valheim/server.conf
+export HOME="/home/slavoie"
+docker run --rm --name valheim-server --cap-add=sys_nice --stop-timeout 120 \
        -p 2456-2457:2456-2457/udp  -v "$HOME"/valheim/config:/config  -v "$HOME"/valheim/data:/opt/valheim \
-       -e SERVER_NAME="Bueno" -e WORLD_NAME="Bueno" -e SERVER_PASS="RVmdCQjBAsAa9sDJ" \
-       -e SERVER-PUBLIC=true ghcr.io/lloesche/valheim-server
+       -e SERVER_NAME="Bueno" -e WORLD_NAME="Bueno" -e SERVER_PASS="$SERVER_PASS" \
+       -e SERVER-PUBLIC="true" -e BACKUPS_MAX_AGE="28" ghcr.io/lloesche/valheim-server
