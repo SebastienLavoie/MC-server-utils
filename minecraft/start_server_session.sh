829e75a328120be76b511931859b0729359ab261
slavoie <slavoie689@gmail.com>
2022-03-19 19:19:00 -0400

    minecraft start server: don't rename window

--- minecraft/start_server_session.sh
+++ minecraft/start_server_session.sh
@@ -6,7 +6,6 @@ SERVER_DIR="$(dirname "$SERVER_START_SCRIPT")"
 
 echo "Starting server in session $SERVER_TMUX"
 tmux new-session -d -s "$SERVER_TMUX"
-tmux rename-window -t "$SERVER_TMUX".0 'server'
 send_server_cmd "cd $SERVER_DIR"
 send_server_cmd "$SERVER_START_SCRIPT"
 sleep 30
