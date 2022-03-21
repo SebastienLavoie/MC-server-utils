7d46332d73c693cadf247509ea9f5b13fa0e8261
slavoie <slavoie689@gmail.com>
2022-03-20 20:11:47 -0400

    check that backup dir exists before backing up

--- minecraft/sync-backups.sh
+++ minecraft/sync-backups.sh
@@ -9,6 +9,11 @@ BACKUP_LOG_DIR="$SERVER_DIR"/backup-sync-logs
 BACKUP_TIME=$(date +%Y%m%dT%H%M%S)
 BACKUP_LOG="$BACKUP_LOG_DIR"/"$BACKUP_TIME"-backup-sync.log
 
+if [[ ! -d "$FTB_BACKUP_DIR" ]]; then
+    echo "$FTB_BACKUP_DIR does not exist, not backing up"
+    exit 0
+fi
+
 mkdir -p "$BACKUP_LOG_DIR"
 exec > >(tee -a "$BACKUP_LOG")
 exec 2> >(tee -a "$BACKUP_LOG" >&2)
