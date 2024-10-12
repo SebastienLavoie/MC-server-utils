68cbbfe1a1e357ff1e579b8417b151737a53c33e
Sebastien Lavoie <slavoie689@gmail.com>
2024-10-12 19:21:09 -0400

    minecraft: add possibility of dev server service

--- minecraft/kill_server_session.sh
+++ minecraft/kill_server_session.sh
@@ -2,8 +2,13 @@
 
 set -o errexit
 
+if [[ -z ${1+x} ]]; then
+	echo "Missing server config file name"
+	exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source "$SCRIPT_DIR"/server.conf
+source "$SCRIPT_DIR"/"$1"
 
 echo "Killing server"
 send_server_cmd '/save-all'
@@ -12,7 +17,7 @@ server_say 'SHUTTING DOWN'
 sleep 2
 send_server_key 'C-c'
 echo "Server killed"
-sleep 40
+sleep 20
 
 tmux kill-session -t "$SERVER_TMUX"
 echo "Session killed"
--- minecraft/start_server_session.sh
+++ minecraft/start_server_session.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ -z ${1+x} ]];then
+	echo "Missing server config file name"
+	exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source "$SCRIPT_DIR"/server.conf
+source "$SCRIPT_DIR"/"$1"
 SERVER_DIR="$(dirname "$SERVER_START_SCRIPT")"
 
 echo "Starting server in session $SERVER_TMUX"
