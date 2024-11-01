71bedd80c350b0c4e655e178a3e7de87b74d7fd5
Sebastien Lavoie <slavoie689@gmail.com>
2024-10-31 20:42:49 -0400

    start_server_session: sleep after creating tmux session

--- minecraft/start_server_session.sh
+++ minecraft/start_server_session.sh
@@ -11,6 +11,7 @@ SERVER_DIR="$(dirname "$SERVER_START_SCRIPT")"
 
 echo "Starting server in session $SERVER_TMUX"
 tmux new-session -d -s "$SERVER_TMUX"
+sleep 2
 send_server_cmd "cd $SERVER_DIR"
 send_server_cmd "$SERVER_START_SCRIPT"
 sleep 30
