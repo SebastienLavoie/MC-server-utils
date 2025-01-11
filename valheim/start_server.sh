1da3de5a45da59affbc6e41e12336aa2fa79ab6d
Sebastien Lavoie <slavoie689@gmail.com>
2025-01-11 11:24:55 -0500

    valheim: update server conf and start script

--- valheim/start_server.sh
+++ valheim/start_server.sh
@@ -3,8 +3,6 @@
 set -eEuo pipefail
 
 source /home/slavoie/valheim/server.conf
-export HOME="/home/slavoie"
 docker run --rm --name valheim-server --cap-add=sys_nice --stop-timeout 120 \
-       -p 2456-2457:2456-2457/udp  -v "$HOME"/valheim/config:/config  -v "$HOME"/valheim/data:/opt/valheim \
-       -e SERVER_NAME="Bueno" -e WORLD_NAME="Bueno" -e SERVER_PASS="$SERVER_PASS" \
-       -e SERVER-PUBLIC="true" -e BACKUPS_MAX_AGE="28" ghcr.io/lloesche/valheim-server
+       -p 2456-2457:2456-2457/udp  -v "$SERVER_DIR"/config:/config  -v "$SERVER_DIR"/data:/opt/valheim \
+       -e SERVER_PASS="$SERVER_PASS" -e SERVER-PUBLIC="true" -e BACKUPS_MAX_AGE="28" ghcr.io/lloesche/valheim-server
