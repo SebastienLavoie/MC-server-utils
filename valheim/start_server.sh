9209f514375c77da9f58e2fc04c4a0e4c06f2c34
Sebastien Lavoie <slavoie689@gmail.com>
2025-01-11 11:32:08 -0500

    valheim: add hidden file for server pass

--- valheim/start_server.sh
+++ valheim/start_server.sh
@@ -2,7 +2,10 @@
 
 set -eEuo pipefail
 
-source /home/slavoie/valheim/server.conf
+source /home/slavoie/Valheim/server.conf
+source /home/slavoie/Valheim/.server.pass
+
 docker run --rm --name valheim-server --cap-add=sys_nice --stop-timeout 120 \
        -p 2456-2457:2456-2457/udp  -v "$SERVER_DIR"/config:/config  -v "$SERVER_DIR"/data:/opt/valheim \
-       -e SERVER_PASS="$SERVER_PASS" -e SERVER-PUBLIC="true" -e BACKUPS_MAX_AGE="28" ghcr.io/lloesche/valheim-server
+       -e SERVER_NAME="Seb's Server" -e WORLD_NAME="Valhalla" \
+       -e SERVER_PASS="$SERVER_PASS" -e BACKUPS_MAX_AGE="28" ghcr.io/lloesche/valheim-server
